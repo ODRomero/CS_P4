e4fe99a724217f356532ec9326cbc8441f7f3f33
Oscar Romero <odromerohumber@alu.ucam.edu>
2023-01-05 12:29:52 +0100

    añadimos un par de lineas en el main

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,4 +1,4 @@
-
+import org.junit.Before;
 
 public class Principal {
 
@@ -8,7 +8,11 @@ public class Principal {
 
 		System.out.println("Manzanas por colores");
 		System.out.println("---------------------");
-		
+		Manzanas m1;
+		Manzanas m2;
+		 
+		m1= new Manzanas(4.5f,"verde");
+		m2= new Manzanas(4.5f,"roja");
 		
 		
 	}
